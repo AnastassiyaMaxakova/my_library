4b779eedd59adcba5833a4610e4903dd6b27d3bf
AnastassiyaMaxakova <anastassiya.maxakova@gmail.com>
2024-06-07 17:36:35 +0500

    Add functionality to addBookToLibrary function, clearForm function

--- script.js
+++ script.js
@@ -6,19 +6,18 @@ const myLibrary = [];
 
 addBtn.addEventListener('click', () => modal.showModal());
 closeBtn.addEventListener('click', ()=> modal.close());
-addBookBtn.addEventListener('click', addBookToLibrary());
+addBookBtn.addEventListener('click', addBookToLibrary);
 
 
 function addBookToLibrary(){
-    /*let title = document.getElementById('title').value;
+    let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
     let readStatus = document.getElementById('checkbox').checked;
-    myLibrary.push(new Book(title,author,pages,readStatus));*/
+    myLibrary.push(new Book(title,author,pages,readStatus));
     
-
-
-    //addBookBtn.addEventListener('click',()=>console.log(readStatus));
+    clearForm();
+    modal.close();
 
 }
 
@@ -27,4 +26,11 @@ function Book(title,author,pages,readStatus){
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+}
+
+function clearForm(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    document.getElementById('checkbox').checked = false;
 }
