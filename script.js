dcd909fd896fe97d6855d90584f4c1f4323c07e5
AnastassiyaMaxakova <anastassiya.maxakova@gmail.com>
2024-06-11 19:12:48 +0500

    Add remove feautre to a display book function

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const closeBtn = document.getElementById('closeBtn');
 const addBookBtn = document.getElementById('addBookBtn');
 const myLibrary = [];
 const bookContainer = document.getElementsByClassName('bookContainer');
+const removeBtn = document.getElementById('removeBtn')
+const main = document.querySelector('main');
+let num = 0;
 
 addBtn.addEventListener('click', () => modal.showModal());
 closeBtn.addEventListener('click', ()=> modal.close());
 addBookBtn.addEventListener('click', addBookToLibrary);
-//addBookBtn.addEventListener('click', displayBook)
-//removeBtn.addEventListener('click',remove)
+
 
 
 function addBookToLibrary(){
@@ -45,23 +47,29 @@ function clearForm(){
     document.getElementById('checkbox').checked = false;
 }
 
-const main = document.querySelector('main');
-
 function displayBook(){
-            let i = myLibrary.length-1;
-            let bookDiv = document.createElement('div');
-            bookDiv.classList.add('bookContainer');
-            main.appendChild(bookDiv);
+        let i = myLibrary.length-1;
+        let bookDiv = document.createElement('div');
+        bookDiv.classList.add('bookContainer');
+        main.appendChild(bookDiv);
 
-            let bookInfo =
-            `<button class="removeBtn" id="removeBtn">x</button>
+        let bookInfo =
+        
+            `<button class="removeBtn" id="removeBtn-${num}">x</button>
                 <div class="bookInfo">
                     <p class="title">Title:${myLibrary[i].title}</p>
                     <p class="author">Author:${myLibrary[i].author}</p>
                     <p class="pages">Pages:${myLibrary[i].pages}</p>
                     <p class="readStatus">Read:${myLibrary[i].readStatus=== false? 'No' : 'Yes'}</p>
                 </div> `;
-            bookDiv.insertAdjacentHTML('beforeend',bookInfo)
+        bookDiv.insertAdjacentHTML('beforeend',bookInfo);
+
+        document.querySelector('#removeBtn-'+num)
+        .addEventListener('click', function(){
+            let removeEl = this.parentNode;
+            main.removeChild(removeEl)
+        });
+        num++;
 }     
 
 
